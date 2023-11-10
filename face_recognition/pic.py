8a5b152e3fc6189008024e07cfa453439a7e5e86
Bodhisattwa Das <bodhi140802@gmail.com>
2023-11-10 09:55:37 +0530

    Modified Face Recognition Code

--- face_recognition/pic.py
+++ face_recognition/pic.py
@@ -5,20 +5,14 @@ cam = cv2.VideoCapture(0)
 
 id = input("Enter your Roll No : ")
 main_folder = os.getcwd()
-# dir = os.getcwd()
-# Path = os.path.join(dir , 'photos ' , id )
 os.mkdir('photos/' + id)
 sampleNum =  0
 
 while(True):
     ret , frame = cam.read()
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     faces = detector.detectMultiScale(frame , 1.3 , 5)
     for( x, y ,w ,h) in faces:
         cv2.rectangle(frame , (x,y) , (x+w ,y+h) , (255,0,0) , 2)
-        
-        
-        # saving the images in the folder 
         folder_name = os.path.join(main_folder , 'photos', id)
         file_name = id+ '.' + str(sampleNum) + '.jpg'
         file_path = os.path.join(folder_name , file_name)
--- file-opener.py
+++ file-opener.py
@@ -4,18 +4,22 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 
 def open_image():
-  global image_object
-  filepath = filedialog.askopenfilename(title="Open Image", filetypes=[("Image Files", "*.jpg *.png *.jpeg")])
-  if filepath:
-    image = Image.open(filepath)
-    resized_image = image.resize((400, 200), Image.ANTIALIAS)
-    photoimage = ImageTk.PhotoImage(resized_image)
+    global image_object
+    filepath = filedialog.askopenfilename(title="Open Image", filetypes=[("Image Files", "*.jpg *.png *.jpeg")])
+    if filepath:
+        image = Image.open(filepath)
+        resized_image = image.resize((400, 200), Image.ANTIALIAS)
+        photoimage = ImageTk.PhotoImage(resized_image)
 
-    if image_object is not None:
-      image_object.destroy()
+        if image_object is not None:
+            image_object.destroy()
 
-    image_object = photoimage
-    image_label.configure(image=photoimage)
+        image_object = photoimage
+        image_label.configure(image=photoimage)
+
+def update_table():
+    predicted_roll_no = "NULL"  
+    table_value.set(predicted_roll_no)
 
 root = tk.Tk()
 root.title("Prototype Demo")
@@ -32,12 +36,25 @@ style = ttk.Style()
 style.configure('TButton', font=('Helvetica', 12, 'bold'))
 
 style.configure(
-  'Browse.TButton',
-  foreground='white', 
-  background='#007bff', 
+    'Browse.TButton',
+    foreground='white',
+    background='#007bff',
 )
 
 open_button = ttk.Button(root, text="Browse", command=open_image, style='TButton')
 open_button.pack(pady=10)
+
+table_frame = ttk.Frame(root)
+table_frame.pack(pady=10)
+
+left_cell_label = ttk.Label(table_frame, text="Predicted Roll No:", font=("Helvetica", 10, "bold"))
+left_cell_label.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
+
+table_value = tk.StringVar()
+right_cell_label = ttk.Label(table_frame, textvariable=table_value, font=("Helvetica", 10, "bold"))
+right_cell_label.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+
+
+update_table()
 
 root.mainloop()
